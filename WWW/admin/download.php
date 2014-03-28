76009f9c8eb1ae434068636a21d6938db28855c6
Michael Divitto <n02482434@hawkmail.newpaltz.edu>
2014-03-28 14:58:40 -0400

    Added the new client installer and updated the download page

--- WWW/admin/download.php
+++ WWW/admin/download.php
@@ -4,12 +4,17 @@ function customPageHeader(){?>
 <?php }
 include_once('header.php');
 ?>
-	<p>Client - Windows</p>
+	<p>New Client</p>
+	<ul>
+		<li>setup.exe - Extracts required files to C:\openlabs and registers the client in the Task Scheduler to run when any user logs in.</li>
+	</ul>
+	<p>Client - Windows [DEPRECATED]</p>
 	<ul>
 		<li>client - this will open in a console window and update the server saying the computer is unavailable (in use).</li>
 		<li>run.vbs - this will allow the program to run in the background with no window (user does not know it is running).
 		<ul><li>Can be useful if used as a login script.</li></ul></li>
 		<li>Now has the Ability to Ping to a specified server.</li>
 	</ul>
-	<a href="../client/client2.zip"><p>Download Client</a></p>
+	<a href="../client/setup.exe"><p>Download Client [3/28/2014]</a></p>
+	<a href="../client/client2.zip"><p>Download Client - DEPRECATED</a></p>
 <?php include_once('footer.php'); ?>
