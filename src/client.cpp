eda25fc565c912b53056e9caaf3a2323d1f79627
Michael Divitto <n02482434@hawkmail.newpaltz.edu>
2014-05-05 14:04:37 -0400

    fixes #20 ; added the break statements to the other handlers to prevent the program from quitting when it is unable to bind itself, etc.

--- src/client.cpp
+++ src/client.cpp
@@ -10,26 +10,28 @@ int main(int argc, char *argv[])
 			printf("Usage: client.exe [Host IP Addr.] [Host domain] [Directory on domain]\n");
 			break;
 		}
-		while (true){			
+		while (true){
 			WSADATA wsa;
 			SOCKET s;
 			struct sockaddr_in server;
 			char message[500], server_reply[2000], hname[100], *get, *host, *conn, tmp[100];
 			int recv_size, timevar;
 			DWORD nHostName = sizeof(hname);
+			//get = "GET /~n02482434/openlabs/";
+			//host = "Host: cs.newpaltz.edu\r\n";
 			conn = "Connection: close\r\n\r\n";
 			GetComputerName(hname, &nHostName);
 			timevar = time(NULL);
 			sprintf_s(tmp, "%d", timevar);
 			sprintf_s(message, "GET %sfunc/update.php?hname=%s&tstamp=%s HTTP/1.1\r\nHost: %s\r\n%s", argv[3], hname, tmp, argv[2], conn);
-			if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0){break;}
-			if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET){}
+			if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0){ break; }
+			if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET){ break; }
 			server.sin_addr.s_addr = inet_addr(argv[1]);
 			server.sin_family = AF_INET;
 			server.sin_port = htons(80);
-			if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0){break;}
-			if (send(s, message, strlen(message), 0) < 0){break;}
-			if ((recv_size = recv(s, server_reply, 2000, 0)) == SOCKET_ERROR){}
+			if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0){ break; }
+			if (send(s, message, strlen(message), 0) < 0){ break; }
+			if ((recv_size = recv(s, server_reply, 2000, 0)) == SOCKET_ERROR){ break; }
 			server_reply[recv_size] = '\0';
 			printf(server_reply);
 			closesocket(s);
